720d3f3b613f1022fce5ce1056821a28300d5be4
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-22 21:48:31 +0100

    Finalisation du programme

--- Maths_expertes/coef_de_bezou.py
+++ Maths_expertes/coef_de_bezou.py
@@ -18,13 +18,14 @@ def bezou(a,b):
     # Crée une liste dernier reste qui contient le point de départ des calculs
     dernier_reste = [liste_div_eucl[indice][0], liste_div_eucl[indice][1], liste_div_eucl[indice][2]]
     indice -= 1
-    print(dernier_reste)
-    while indice > 0:
+    while indice >= 0:
         # Fait une liste des éléments pour le calcul de reste
         calcul_reste = [liste_div_eucl[indice][1], liste_div_eucl[indice][2]]
+        
+        # Partie remplacement
         # Cherche où remplacer par les calculs de reste
         indice_remplacement = 2
-        if dernier_reste[1][1] == liste_div_eucl[indice][2]:
+        if dernier_reste[2][1] == liste_div_eucl[indice][2][1]:
             indice_remplacement = 1
         # Définit le multiplicateur pour multiplier les quotients des nombres du calcul du reste
         multiplicateur = dernier_reste[indice_remplacement][0]
@@ -32,8 +33,16 @@ def bezou(a,b):
         calcul_reste = [[multiplicateur*calcul_reste[0][0],calcul_reste[0][1]], [multiplicateur*calcul_reste[1][0],calcul_reste[1][1]]]
         # Met à jour le calcul du dernier reste
         dernier_reste[indice_remplacement] = calcul_reste
-        print(dernier_reste)
+        
+        # Partie combinaison des différents éléments
+        indice_combinaison = 1
+        if indice_remplacement == 1:
+            indice_combinaison = 2
+        if dernier_reste[indice_combinaison][1] == dernier_reste[indice_remplacement][0][1]:
+            dernier_reste[indice_combinaison] = [dernier_reste[indice_combinaison][0] + dernier_reste[indice_remplacement][0][0]]
+            dernier_reste[indice_remplacement] = dernier_reste[indice_remplacement][1]
+        else:
+            dernier_reste[indice_combinaison][0] = dernier_reste[indice_combinaison][0] + dernier_reste[indice_remplacement][1][0]
+            dernier_reste[indice_remplacement] = dernier_reste[indice_remplacement][0]
         indice -= 1
-    return liste_div_eucl
-
-bezou(559, 325)
+    return dernier_reste
