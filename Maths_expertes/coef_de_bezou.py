6638a7d9e2b619a6586e3a6bd41f99be72ac9d20
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-22 20:01:39 +0100

    Création de la fonction avec sa docstring

---
+++ Maths_expertes/coef_de_bezou.py
@@ -0,0 +1,8 @@
+def bezou(a,b):
+    """Fonction retournant les coefs de Bézou.
+
+    Args:
+        a (integer): Premier nombre, il doit être divisible par b
+        b (integer): Second nombre, il est le diviseur de a
+    """
+    return None
