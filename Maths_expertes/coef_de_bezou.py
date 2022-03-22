431ea952949c2f8fe463c76d8b01b696e327e852
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-22 21:10:37 +0100

    Ajout de la recherche de reste

--- Maths_expertes/coef_de_bezou.py
+++ Maths_expertes/coef_de_bezou.py
@@ -16,9 +16,17 @@ def bezou(a,b):
     # Calcul des coefs de Bézou
     indice = len(liste_div_eucl) - 1
     # Crée une liste dernier reste qui contient le point de départ des calculs
-    dernier_reste = [liste_div_eucl[indice][2], liste_div_eucl[indice][0], liste_div_eucl[indice][2]]
+    dernier_reste = [liste_div_eucl[indice][2], liste_div_eucl[indice][0], liste_div_eucl[indice][1]]
     indice -= 1
+    print(dernier_reste)
     while indice > 0:
+        # Fait une liste des éléments pour le calcul de reste
+        calcul_reste = [liste_div_eucl[indice][0], liste_div_eucl[indice][1]]
+        # Cherche où remplacer par les calculs de reste
+        if dernier_reste[1][1] == liste_div_eucl[indice][2]:
+            dernier_reste[1][1] = calcul_reste
+        else:
+            dernier_reste[2][1] = calcul_reste
         indice -= 1
     return liste_div_eucl
 
