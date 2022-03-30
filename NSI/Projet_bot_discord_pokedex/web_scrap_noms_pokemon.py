d824cdb05c2d8c74c250ee161eb86d7881a67bb9
RisingSunLight <risingsunlight5776@gmail.com>
2022-03-30 17:58:25 +0200

    Modification du script pour qu'il s'adapte dans les cas des formes particulières

--- NSI/Projet_bot_discord_pokedex/web_scrap_noms_pokemon.py
+++ NSI/Projet_bot_discord_pokedex/web_scrap_noms_pokemon.py
@@ -13,11 +13,16 @@ if response.ok:
     tr = body.findAll("tr") # Récupère mes row de la table
     dico_noms_langues = {} # Initialise le dictionnaire
     # Parcours la liste des rangées par élément pour obtenir chaque case françaises et anglaise et obtenir le nom du pokemon
-    for i in range(1, len(tr) - 1):
+    for i in range(1, len(tr)):
         td = tr[i].findAll("td")
+        decalage = 0
+        numero = td[0].getText().replace("\n", "")
+        # Si jamais la case 1 n'est pas un numéro, alors c'est une forme alternative, tous les éléments sont décalés d'un
+        if (not numero.isdigit() and numero != "—"): # Les cases avec le signe "-" sont celles qui n'ont pas de numéros, mais qui ne sont pas des formes alternatives
+            decalage = 1 # On attribue donc un décalage de 1
         # Récupère les noms sans les retours à la ligne
-        nom_fr = td[2].getText().replace("\n", "")
-        nom_en = td[3].getText().replace("\n", "")
+        nom_fr = td[2 - decalage].getText().replace("\n", "")
+        nom_en = td[3 - decalage].getText().replace("\n", "")
         if nom_fr not in dico_noms_langues.keys(): # Si le nom_fr n'est pas présent dans les clés du dico, l'ajoute avec en valeur nmom_en
             dico_noms_langues[nom_fr] = nom_en
     # Push le dictionnaire dans le fichier JSON
