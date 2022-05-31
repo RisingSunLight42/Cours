c9a99ce35a14258c36c2722c83b13ecd85ad1720
RisingSunLight <85577959+RisingSunLight42@users.noreply.github.com>
2022-05-31 09:00:04 +0200

    Mise à jour du programme

--- NSI/Projet_bot_discord_pokedex/bot.py
+++ NSI/Projet_bot_discord_pokedex/bot.py
@@ -4,30 +4,19 @@ from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_slash.utils.manage_commands import create_option
 from prog_api import info_pokemon
+import csv
 
 # Création de l'objet bot avec pour prefix "poke!" et ajout d'un objet slash
 bot = commands.Bot(command_prefix='poke!')
 slash = SlashCommand(bot, sync_commands=True)
 
 # Initialisation dictionnaire des couleurs de type
-dico_type_couleur = {"Normal": 0xA8A77A,
-                     "Fire": 0xEE8130,
-                     "Water": 0x6390F0,
-                     "Electric": 0xF7D02C,
-                     "Grass": 0x7AC74C,
-                     "Ice": 0x96D9D6,
-                     "Fighting": 0xC22E28,
-                     "Poison": 0xA33EA1,
-                     "Ground": 0xE2BF65,
-                     "Flying": 0xA98FF3,
-                     "Psychic": 0xF95587,
-                     "Bug": 0xA6B91A,
-                     "Rock": 0xB6A136,
-                     "Ghost": 0x735797,
-                     "Dragon": 0x6F35FC,
-                     "Dark": 0x705746,
-                     "Steel": 0xB7B7CE,
-                     "Fairy": 0xD685AD,}
+dico_type_couleur = {}
+with open("trad_types_et_code_couleur.csv", "r") as fichierDicoTypes:
+    lecteur = csv.reader(fichierDicoTypes, delimiter=";")
+    for ligne in lecteur:
+        dico_type_couleur[ligne[0]] = (ligne[1], ligne[2])
+
 
 # Récupération du token de connection à partir du fichier texte
 token = ""
@@ -40,6 +29,7 @@ with open("token.txt", "r") as fichierToken:
 async def on_ready():
         print(f'Le bot est bien connecté.')
 
+
 # Fonction créant l'embed avec les infos du pokemon
 async def embed_pokemon_pokedex(ctx, nom):
     dico = info_pokemon(nom)
@@ -49,12 +39,15 @@ async def embed_pokemon_pokedex(ctx, nom):
     types = dico['types']
     str_type = "Type"
     if len(types) > 1: str_type += "s"
+    couleur_embed = int(dico_type_couleur[types[0]][1], 16) # Récupère la couleur correspond au type d'indice 0 renvoyé par les infos
+    for i in range(len(types)): # Fait une boucle pour remplacer chaque type par leur version française
+        types[i] = dico_type_couleur[types[i]][0]
     
     # Création de l'embed
     embed = discord.Embed(title=f"{dico['nom'].capitalize()} (#{dico['numero']})", # .capitalize() met la première lettre en maj
-                          color=dico_type_couleur[types[0]]) # Récupère la couleur correspond au type d'indice 0 renvoyé par les infos
-    embed.add_field(name="Taille", value=f"{dico['taille']} ft")
-    embed.add_field(name="Poids", value=f"{dico['poids']} lbs")
+                          color=couleur_embed) 
+    embed.add_field(name="Taille", value=f"{dico['taille']} m")
+    embed.add_field(name="Poids", value=f"{dico['poids']} kg")
     embed.add_field(name=str_type, value=f"{', '.join(types)}", inline=False)
     embed.set_image(url=dico["image"])
     await ctx.send(embed=embed)
@@ -75,12 +68,14 @@ async def info(ctx, nom):
 async def infoPokemon(ctx, nom):
     await embed_pokemon_pokedex(ctx, nom)
 
+
 # Gestion des erreurs de la commande infoPokemon
 @info.error
 async def info_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.message.delete() # Supprime le message de commande
         await ctx.send("Tu ne m'as pas donné de nom ! Je ne peux pas faire de recherches dans le pokedex sans !")
+
 
 # Lancement du bot
 bot.run(token)
