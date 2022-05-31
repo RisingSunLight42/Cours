7fc8c91c9e83d2f1546916b18281266d53176ab7
RisingSunLight <85577959+RisingSunLight42@users.noreply.github.com>
2022-05-31 09:02:14 +0200

    Mise à jour du programme

--- NSI/Projet_bot_discord_pokedex/prog_api.py
+++ NSI/Projet_bot_discord_pokedex/prog_api.py
@@ -1,31 +1,48 @@
 import requests
+import json
+
+def traduction_nom(nom):
+    '''Fonction qui prend en argument le nom d'un pokemon en français ou en anglais, et
+       qui renvoie les noms anglais et français du pokemon.
+       Exemple : >>> traduction_nom('dracaufeu')
+       renvoie :
+       [Dracaufeu, Charizard] '''
+    nom = nom.lower()   # On met le nom en minuscule
+    with open('noms_pokemon_fr_en.json', encoding = 'utf-8') as fichierJSON :
+        contenu = json.load(fichierJSON)
+        for cle, valeur in contenu.items(): # On cherche si le nom est présent dans le ficher json
+            if nom in [cle, valeur]:
+                return [cle, valeur]        # Si oui on renvoie la valeur et la clé
+        return False                        # Sinon on renvoie False
+
 
 def info_pokemon(nom):
-    ''' Fonction qui prend en argument le nom d'un pokemon (en anglais), et
+    ''' Fonction qui prend en argument le nom d'un pokemon (en français ou en anglais), et
         qui renvoie un dictionnaire contenant le numéro, la taille, le poid, les types et l'url d'une image.
-        Exemple : >>> info_pokemon(charizard)
+        Exemple : >>> info_pokemon('dracaufeu')
         renvoie :
-        {'numero' : 6, 'nom' : 'charizard','taille' : '17','poids' : '905',
-        'image' : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-        'types': ['Flying','Fire']}'''
-    nom = nom.lower()
-    url = f"https://pokeapi.co/api/v2/pokemon/{nom}/" # On met le nom du pokemon dans l'url
+        {'numero': 6, 'nom': 'Dracaufeu', 'taille': '5.2', 'poids': '410.5',
+        'image': 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+        'types': ['Fire', 'Flying']} '''
+    nom = traduction_nom(nom)
+    if not nom:                      # Si nom vaut False, alors la clé est incorrecte 
+        return None                  # on renvoie None
+    url = f"https://pokeapi.co/api/v2/pokemon/{nom[1]}/" # Sinon on met le nom du pokemon dans l'url
     
     reponse = requests.get(url)      # On recupère les informations dans l'API
-    if reponse.status_code == 404:   # Si le nom est mauvais, on a l'erreur 404
-        return None                  # donc on renvoie None
-    contenu_txt = reponse.text       # Si c'est bon on met
-    contenu = reponse.json()         # tout sous forme de dictionnaire
+    contenu_txt = reponse.text       
+    contenu = reponse.json()         
+    
     infos  = {
-        'numero' : contenu["id"],    # On crée le dictionnaire
-        'nom' : contenu["name"],
-        'taille' : contenu["height"],
-        'poids' : contenu["weight"],
-        'image' : contenu["sprites"]["front_default"]
+        'numero' : contenu["id"],    # On crée le dictionnaire avec les informations
+        'nom' : nom[0].capitalize(),
+        'taille' : str(round(int(contenu["height"]) * 0.1, 2)),
+        'poids' : str(round(int(contenu["weight"]) * 0.1, 2)),
+        'image' : contenu["sprites"]["other"]["official-artwork"]["front_default"]
         }
     liste_types = []
     for i in range(len(contenu["types"])):      # On y ajoute les types
-        letype = contenu["types"][i-1]['type']['name']
+        letype = contenu["types"][i]['type']['name']
         liste_types.append(letype.capitalize())
     infos['types'] = liste_types
     return infos                     # Et on le renvoie
