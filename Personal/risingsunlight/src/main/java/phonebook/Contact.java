c625188e316ea032efd7aa097d471bb329f4b2b6
RisingSunLight <risingsunlight5776@gmail.com>
2022-07-07 17:52:47 +0200

    ➕Add Contact class

---
+++ Personal/risingsunlight/src/main/java/phonebook/Contact.java
@@ -0,0 +1,33 @@
+package phonebook;
+
+public class Contact {
+
+    private String lastName;
+    private String firstName;
+
+    private Long phoneNumber;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
