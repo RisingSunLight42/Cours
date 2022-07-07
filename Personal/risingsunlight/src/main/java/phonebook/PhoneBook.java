b8d7f8377d675536d5478d750f8247cdf60e07ea
RisingSunLight <risingsunlight5776@gmail.com>
2022-07-07 17:47:15 +0200

    🔨Add userInput

--- Personal/risingsunlight/src/main/java/phonebook/PhoneBook.java
+++ Personal/risingsunlight/src/main/java/phonebook/PhoneBook.java
@@ -1,11 +1,29 @@
 package phonebook;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Basé sur la vidéo tuto https://www.youtube.com/watch?v=DTYjbHC0rj8
  *
  */
 public class PhoneBook {
+
+    public static Scanner userInputScanner = null;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        userInputScanner = new Scanner(System.in);
+
+        // * Récupère les infos utilisateurs
+        String lastName = getUserInput("Entrez le nom de famille :");
+        String firstName = getUserInput("Entrez le prénom :");
+        String phoneNumber = getUserInput("Entrez le numéro de téléphone :");
+
+        userInputScanner.close(); // Ferme le scanner, doit être fermé pour des soucis de perfs
+    }
+
+    public static String getUserInput(String message) {
+        System.out.println(message);
+        return userInputScanner.nextLine();
     }
 }
