45eb7ba297cf5c9e4fa25228eb3f19d59c92f2fc
RisingSunLight <risingsunlight5776@gmail.com>
2022-07-07 18:43:44 +0200

    ➕Refacto to add getOrCreatePhoneBookFile method

--- Personal/risingsunlight/src/main/java/phonebook/PhoneBook.java
+++ Personal/risingsunlight/src/main/java/phonebook/PhoneBook.java
@@ -27,32 +27,41 @@ public class PhoneBook {
 
         Contact newContact = new Contact(lastName, firstName, phoneNumber); // Crée un nouveau contact
 
-        // Récupère le fichier du phonebook
-        File phoneBookFile = new File(
-                "Personal\\risingsunlight\\src\\main\\java\\phonebook\\phonebook.txt");
+        File phoneBookFile = getOrCreatePhoneBookFile(
+                "Personal\\risingsunlight\\src\\main\\java\\phonebook\\phonebook.txt"); // Récupère le fichier phonebook
 
-        if (phoneBookFile.exists()) {
-            try {
-                BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true));
-                fileWriter.append(newContact.toString());
-                System.out.println("Contact ajouté !");
-                fileWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                phoneBookFile.createNewFile();
-                System.out.println("Le fichier a bien été créé.");
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Le fichier n'existe pas et je n'ai pas pu le créer.");
-            }
+        try {
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true));
+            fileWriter.append(newContact.toString() + "\n");
+            System.out.println("Contact ajouté !");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
     public static String getUserInput(String message) {
         System.out.println(message);
         return userInputScanner.nextLine();
+    }
+
+    public static File getOrCreatePhoneBookFile(String phoneBookFilePath) {
+        File phoneBookFile = new File(phoneBookFilePath);
+
+        if (phoneBookFile.exists()) {
+            return phoneBookFile;
+        }
+
+        try {
+            phoneBookFile.createNewFile();
+            System.out.println("Le fichier a bien été créé à l'emplacement donné (" + phoneBookFilePath + ")");
+            return phoneBookFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Le fichier n'existe pas et je n'ai pas pu le créer.");
+        }
+
+        return null;
     }
 }
