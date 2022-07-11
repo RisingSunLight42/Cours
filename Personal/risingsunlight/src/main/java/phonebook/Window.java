ca5ea0effae640468d26f671b31580933f25de83
RisingSunLight <risingsunlight5776@gmail.com>
2022-07-11 15:49:43 +0200

    🐛 change to extend the JFrame class

--- Personal/risingsunlight/src/main/java/phonebook/Window.java
+++ Personal/risingsunlight/src/main/java/phonebook/Window.java
@@ -2,19 +2,20 @@ package phonebook;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.plaf.ColorUIResource;
 
-public class Window {
+public class Window extends JFrame {
     public Window() {
         // JFrame = a GUI window to add components to
 
-        JFrame frame = new JFrame();
-        frame.setTitle("Phonebook");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // by default, it's HIDE_ON_CLOSE
-        frame.setResizable(false);
-        frame.setSize(420, 420);
-        frame.setVisible(true);
+        this.setTitle("Phonebook");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // by default, it's HIDE_ON_CLOSE
+        this.setResizable(false);
+        this.setSize(420, 420);
+        this.setVisible(true);
 
         ImageIcon icon = new ImageIcon("Personal\\risingsunlight\\phonebook_icon.png");
-        frame.setIconImage(icon.getImage());
+        this.setIconImage(icon.getImage());
+        this.getContentPane().setBackground(new ColorUIResource(122, 122, 122));
     }
 }
