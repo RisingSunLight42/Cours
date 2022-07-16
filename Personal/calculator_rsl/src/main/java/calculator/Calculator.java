7911c246e6da7b96c7393a78f080b4651af02b71
RisingSunLight <risingsunlight5776@gmail.com>
2022-07-16 22:11:37 +0200

    🎉 Initial commit

---
+++ Personal/calculator_rsl/src/main/java/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+/**
+ * Hello world!
+ */
+public final class Calculator {
+    private Calculator() {
+    }
+
+    /**
+     * Says hello to the world.
+     *
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
