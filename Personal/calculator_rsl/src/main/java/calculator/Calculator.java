57a41d9d8c1c4022edf8a79ec90eb2f9277ff885
RisingSunLight <risingsunlight5776@gmail.com>
2022-07-16 22:39:43 +0200

    ✅ start of calculator project with simple operations

--- Personal/calculator_rsl/src/main/java/calculator/Calculator.java
+++ Personal/calculator_rsl/src/main/java/calculator/Calculator.java
@@ -1,11 +1,11 @@
 package calculator;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Calculator, do what a calculator do
  */
 public final class Calculator {
-    private Calculator() {
-    }
 
     /**
      * Says hello to the world.
@@ -13,6 +13,36 @@ public final class Calculator {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner userInputScanner = new Scanner(System.in);
+        System.out.println("Donnez-moi le premier nombre de l'opération.");
+        int firstNumber = Integer.parseInt(userInputScanner.nextLine());
+        System.out.println("Donnez-moi le second nombre de l'opération.");
+        int secondNumber = Integer.parseInt(userInputScanner.nextLine());
+        System.out.println("Donnez-moi le signe de l'opération à effectuer");
+        String operationSign = userInputScanner.nextLine();
+        userInputScanner.close();
+
+        int result = simpleOperation(firstNumber, secondNumber, operationSign);
+        System.out.println(
+                "Le résultat de l'opération " + firstNumber + operationSign + secondNumber + " est :\n" + result);
+    }
+
+    private static int simpleOperation(int firstNumber, int secondNumber, String operationSign) {
+        int result = 0;
+        switch (operationSign) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                break;
+        }
+        return result;
     }
 }
